028fa8d08f21677086a3105f2ba80541bcb049d6
Natalia <nasozontova@gmail.com>
2020-12-23 14:10:55 -0500

    adding destination page and updating style.css file

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Automatic Slideshow - change image every 3 seconds
 var myIndex = 0;
+var search = document.getElementById("search");
 carousel();
 
 function carousel() {
@@ -14,46 +15,43 @@ function carousel() {
   setTimeout(carousel, 3000);
 }
 
-
- // Performing an AJAX request with the queryURL
-
- var queryURL = "https://community-open-weather-map.p.rapidapi.com/weather?q=Dubai%2Cuae&lat=0&lon=0&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html";
- 
+var city;
+$("#submit").on("click", function (event) {
+  event.preventDefault();
+  // Performing an AJAX request with the queryURL
+ var city = search.value.trim();
+ console.log(city);
+var queryURL = "https://community-open-weather-map.p.rapidapi.com/weather?q=Dubai%2Cuae&lat=0&lon=0&id=2172797&lang=null&units=imperial&mode=xml%2C%20html";
  
  $.ajax({
     url: queryURL,
     method: "GET",
     headers: { 'x-rapidapi-key': 'f3b91ecaf5msh8a2cb171637c53ap1e6540jsn46fb9786ca95', 'x-rapidapi-host':'community-open-weather-map.p.rapidapi.com' }
   })
-
-  
     // After data comes back from the request
     .then(function(response) {
       console.log(queryURL);
-
       console.log(response);
-      // storing the data from the AJAX request in the results variable
-    //   var results = response.data;
-   console.log("main" + response.coord);
-   $("#temperature").text(JSON.stringify(response.coord));
+   var temperature = $("#temperature").text("Temperature: " + JSON.stringify(response.main.temp));
+   console.log(temperature);
+   console.log(response.main.temp);
     });
+});
+ 
 
-    //new code
-    // window.weatherWidgetConfig =  window.weatherWidgetConfig || [];
-    // window.weatherWidgetConfig.push({
-    //     selector:".weatherWidget",
-    //     apiKey:"f3b91ecaf5msh8a2cb171637c53ap1e6540jsn46fb9786ca95", //Sign up for your personal key
-    //     location:"Dubai, AE", //Enter an address
-    //     unitGroup:"metric", //"us" or "metric"
-    //     forecastDays:5, //how many days forecast to show
-    //     title:"Dubai, AE", //optional title to show in the 
-    //     showTitle:true, 
-    //     showConditions:true
-    // });
    
-    // (function() {
-    // var d = document, s = d.createElement('script');
-    // s.src = 'https://www.visualcrossing.com/widgets/forecast-simple/weather-forecast-widget-simple.js';
-    // s.setAttribute('data-timestamp', +new Date());
-    // (d.head || d.body).appendChild(s);
-    // })();
+
+
+
+
+
+
+
+
+
+
+
+  
+   
+
+    
